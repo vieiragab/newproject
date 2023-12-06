472fdb1306ac48cd0aafc73a1e54826ffb69657a
Gabriella Vieira <90354488+vieiragab@users.noreply.github.com>
2023-12-05 23:06:55 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,15 +3,23 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Vicio from './components/Vicio';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
+import Contact from './components/Contact';
+import Criar from './components/Criar';
+import About from './components/About';
+
 
 
 const App = () => {
+  
   return (
       <BrowserRouter>
       <Navbar/>
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/vicio' element={<Vicio/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/criar' element={<Criar/>}/>
+          <Route path='/contact' element={<Contact/>}/>
         </Routes>
       </BrowserRouter>
       
