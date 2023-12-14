96f7ad956bab63fa4357842fe1f0db0d564ab343
Gabriella Vieira <90354488+vieiragab@users.noreply.github.com>
2023-12-14 15:16:33 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Vicio from './components/Vicio';
-import Home from './components/Home';
-import Navbar from './components/Navbar';
+import About from './components/About';
 import Contact from './components/Contact';
 import Criar from './components/Criar';
-import About from './components/About';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import Post from './components/Post';
+import Vicio from './components/Vicio';
 
 
 
 const App = () => {
+  const [ posts, setPosts ] = useState([]);
   
   return (
       <BrowserRouter>
       <Navbar/>
         <Routes>
-          <Route path='/' element={<Home/>}/>
+          <Route path='/' element={<Home posts={posts}/>}/>
           <Route path='/vicio' element={<Vicio/>}/>
           <Route path='/about' element={<About/>}/>
-          <Route path='/criar' element={<Criar/>}/>
+          <Route path='/criar' element={<Criar posts={posts} setPosts={setPosts}/>}/>
           <Route path='/contact' element={<Contact/>}/>
+          {posts.map(post => <Route path={`/${post.titulo}`} key={post.titulo} element={<Post post={post}/>}/>)}
         </Routes>
       </BrowserRouter>
       
